a2c10752ece55c45fa1d34747b6260383b653efd
codeTIT4N <lokeshkumar636@yahoo.com>
2023-08-18 03:42:40 +0530

    tests

--- src/packages/generateECDSAKeypair.ts
+++ src/packages/generateECDSAKeypair.ts
@@ -39,58 +39,5 @@ export const generateECDSAKeypairBase64: GenerateECDSAKeypairFn = async () => {
     privateKey: finalPrivateKey,
   };
 
-  // ---------------- TEST ---------------- //
-
-  // convert finalPrivateKey to CryptoKey
-  const importedPrivateKey = await crypto.subtle.importKey(
-    "pkcs8",
-    convertBase64ToArrayBuffer(finalPrivateKey),
-    {
-      name: "ECDSA",
-      namedCurve: "P-256",
-    },
-    true,
-    ["sign"],
-  );
-
-  // convert finalPublicKey to CryptoKey
-  const importedPublicKey = await crypto.subtle.importKey(
-    "spki",
-    convertBase64ToArrayBuffer(finalPublicKey),
-    {
-      name: "ECDSA",
-      namedCurve: "P-256",
-    },
-    true,
-    ["verify"],
-  );
-
-  // signing a message
-  const message = "Test message to sign!";
-  const signature = await crypto.subtle.sign(
-    {
-      name: "ECDSA",
-      hash: { name: "SHA-256" },
-    },
-    importedPrivateKey,
-    new TextEncoder().encode(message),
-  );
-
-  // verifying the signature
-  const isVerified = await crypto.subtle.verify(
-    {
-      name: "ECDSA",
-      hash: { name: "SHA-256" },
-    },
-    importedPublicKey,
-    signature,
-    new TextEncoder().encode(message),
-  );
-
-  assert.strictEqual(isVerified, true, "verify failed");
-
-  // ---------------- TEST ---------------- //
-  //
-
   return finalKeys;
 };
